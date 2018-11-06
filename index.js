1944a7564bc5151b259b49648f06ef20e119be32
Rhian Evans <rhian.evans100@btinternet.com>
2018-11-06 19:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers =  ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift (name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name)
+}
+
+function appendDriver(name) {
+  return [...drivers, (name)]
+}
+  
+function prependDriver(name) {
+  return [(name), ...drivers]
+}
+
+function removeLastDriver(name) {
+drivers.slice(0,1);
+  return drivers.length-1
+}
+
+function removeFirstDriver(n) {
+  drivers.splice()
+  const newDrivers = drivers.splice(1,2)
+  return [drivers.length] 
+}
